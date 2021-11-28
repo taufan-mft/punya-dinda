a337fe3c57852181b59d0fee6b39d6dc90388238
Taufan <taufanderazor@gmail.com>
2021-11-29 00:18:51 +0700

    feat: laying foundation

---
+++ punya-dinda/repository/Connection.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Data.OleDb
+
+Module Connection
+    Public Conn As OleDbConnection
+    Public DA As OleDbDataAdapter
+    Public DS As DataSet
+    Public CMD As OleDbCommand
+    Public DM As OleDbDataReader
+
+    Sub koneksiDB()
+        Try
+            Conn = New OleDbConnection("provider=microsoft.ace.oledb.12.0; data source = YangPentingSelesai.accdb")
+            Conn.Open()
+            MsgBox("DB Connected!")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Module
---
+++ punya-dinda/repository/Repository.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Data.OleDb
+
+Public Class Repository
+    Private Shared objRepository As Repository
+    Private Sub New()
+        koneksiDB()
+    End Sub
+
+    Public Shared Function getInstance() As Repository
+        If (objRepository Is Nothing) Then
+            objRepository = New Repository()
+        End If
+        Return objRepository
+    End Function
+
+    Sub nukeTable(tableName As String)
+        Dim sql As String
+        sql = "DELETE FROM " + tableName
+        CMD = New OleDb.OleDbCommand(sql, Conn)
+        DM = CMD.ExecuteReader
+        MsgBox("Data terhapus.")
+    End Sub
+    Sub showData(sql As String, DGV As DataGridView)
+        DA = New OleDb.OleDbDataAdapter(sql, Conn)
+        DS = New DataSet
+        DA.Fill(DS)
+
+        DGV.DataSource = DS.Tables(0)
+        DGV.ReadOnly = True
+
+    End Sub
+End Class
